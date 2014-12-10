1e78c823405ba69c4230f4592893c90bbaf586a7
Eric Sipple <saalon@gmail.com>
2014-12-10 10:42:30 -0500

    Capitan v 1.0.1

--- capitan.gemspec
+++ capitan.gemspec
@@ -4,7 +4,7 @@ require "capitan"
 
 Gem::Specification.new do |s|
   s.name        = "capitan"
-  s.version     = '1.0.0'
+  s.version     = '1.0.1'
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Eric Sipple"]
   s.email       = ["sipple@trustarts.org"]
--- lib/capitan.rb
+++ lib/capitan.rb
@@ -1,4 +1,18 @@
 module Capitan
+  require 'capitan/connection'
+  require 'capitan/exhibition'
+  require 'capitan/non_ticketed_event'
+  require 'capitan/object_initializer'
+  require 'capitan/performance'
+  require 'capitan/production'
+  require 'capitan/show_methods'
+  require 'capitan/shows'
+  require 'capitan/venue'
+  require 'capitan/calendar/calendar_show'
+  require 'capitan/calendar/day'
+  require 'capitan/calendar/monthly'
+  require 'capitan/calendar/week'
+  require 'capitan/exceptions/connection_error'
 
 
 end
