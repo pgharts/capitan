03a4c9e9adc0e0062d5fbe57e786f25ab5e97445
Eric Sipple <saalon@gmail.com>
2014-12-10 10:46:47 -0500

    Fixing gem for 1.0.1

--- lib/capitan/exceptions/connection_error.rb
+++ lib/capitan/exceptions/connection_error.rb
@@ -1,20 +1,23 @@
-class Capitan::Exceptions::ConnectionError < StandardError
+module Exceptions
+  class ConnectionError < StandardError
 
-  def initialize(error)
-    message = error.message
-    message = socket_error_message if error.class == SocketError
-    message = unauthorized_message if error.class == RestClient::Unauthorized
-    super(message)
-  end
+    def initialize(error)
+      message = error.message
+      message = socket_error_message if error.class == SocketError
+      message = unauthorized_message if error.class == RestClient::Unauthorized
+      super(message)
+    end
 
-  private
+    private
 
-  def socket_error_message
-    "Unable to connect to CAPI. Be sure that you have the correct base_url set in config.yml."
-  end
+    def socket_error_message
+      "Unable to connect to CAPI. Be sure that you have the correct base_url set in config.yml."
+    end
 
-  def unauthorized_message
-    "Invalid API key. Make sure a valid key is set for api_key in config.yml."
+    def unauthorized_message
+      "Invalid API key. Make sure a valid key is set for api_key in config.yml."
+    end
+
   end
 
 end
